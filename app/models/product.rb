196aaceb89e4f7b9c69fa9de3381a6f46eb6e90f
shuhei-icb <shuhei.ichiba@icloud.com>
2024-09-22 20:24:45 +0900

    リファクタリング

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,7 @@ class Product < ApplicationRecord
   has_one_attached :image
 
   validates :name, presence: true
-  validates :price,
-            presence: true,
-            numericality: { greater_than: 0 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
 
   scope :default_order, -> { order(:position, :id) }
   acts_as_list
