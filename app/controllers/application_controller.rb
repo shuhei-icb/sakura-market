cc4295e7fdad75666ec1f5fb811d4899d75da260
shuhei-icb <shuhei.ichiba@icloud.com>
2024-09-29 14:25:13 +0900

    ユーザー用のレイアウト追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
+  layout 'layouts/users/application'
+  helper_method :current_cart
+
   def after_sign_in_path_for(resource)
-    admins_root_path
+    resource.instance_of?(Admin) ? admins_root_path : my_page_path
   end
 
   def after_sign_out_path_for(resource)
-    root_path
+    resource.instance_of?(Admin) ? admins_root_path : root_path
+  end
+
+  private
+
+  def current_cart
+    @current_cart ||= current_user&.cart || current_user&.create_cart!
   end
 end
